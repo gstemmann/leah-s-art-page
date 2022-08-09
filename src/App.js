bbc4f2d644776f8cf7b8a96e4c13453c4a9522f5
george <george.stemmann@gmail.com>
2022-08-08 21:54:25 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import logo from './logo.svg';
+import { Route, Routes } from "react-router-dom";
+import { Home, StringArt, PrintMaking, TwoD, ThreeD, Digital, About, Contact, CV } from "./components";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+
+
 import './App.css';
 
 function App() {
@@ -7,9 +12,14 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/string" element={<StringArt/>}/>
+        <Route path="/prints" element={<PrintMaking/>}/>
+        <Route path="/TwoD" element={<TwoD/>}/>
+        <Route path="/ThreeD" element={<ThreeD/>}/>
+        <Route path="/digital" element={<Digital/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/cv" element={<CV/>}/>
       </Routes>
       <Footer/>
     </div>
